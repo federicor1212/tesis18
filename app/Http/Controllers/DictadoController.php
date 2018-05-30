ce9c7fdcd5fcf81fd5e1c03ff68208298a3e9844
Ignacho <ignachobarberis@gmail.com>
2018-05-30 19:14:51 -0300

    correcciones

--- app/Http/Controllers/DictadoController.php
+++ app/Http/Controllers/DictadoController.php
@@ -31,7 +31,7 @@ class DictadoController extends Controller
                  ->join('carreras','materias.id_carrera','=','carreras.id')
                  ->join('alternativas','dictados_clases.id_alternativa','=','alternativas.id')
                  ->join('dias','dictados_clases.id_dia','=','dias.id')
-                 ->select('carreras.id as id_carrera','dictados.id','materias.id AS id_materia','materias.desc_mat','dictados.cuat','dictados.ano','dias.id AS id_dia','dias.descripcion As dia_cursada','alternativas.id AS id_alternativa','alternativas.codigo AS alt_hor','dictados.fecha_inicio','dictados.fecha_fin','dictados.cant_insc_act','dictados.cant_clases','dictados.cant_faltas_max','dictados_clases.id AS id_dictado_clase')
+                 ->select('carreras.id as id_carrera','carreras.desc_carr','dictados.id','materias.id AS id_materia','materias.desc_mat','dictados.cuat','dictados.ano','dias.id AS id_dia','dias.descripcion As dia_cursada','alternativas.id AS id_alternativa','alternativas.codigo AS alt_hor','dictados.fecha_inicio','dictados.fecha_fin','dictados.cant_insc_act','dictados.cant_clases','dictados.cant_faltas_max','dictados_clases.id AS id_dictado_clase')
                  ->orderby('materias.desc_mat', 'dictados.ano', 'dictados.cuat')
                  ->get();
 
