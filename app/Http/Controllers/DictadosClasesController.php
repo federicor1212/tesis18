11813b12ec9812fea44e7b620fe4aabaab2d8937
Ignacho <ignachobarberis@gmail.com>
2018-08-17 18:51:55 -0300

    upd

--- app/Http/Controllers/DictadosClasesController.php
+++ app/Http/Controllers/DictadosClasesController.php
@@ -24,14 +24,20 @@ class DictadosClasesController extends Controller
             }
           }  
 
-        $dictado = Dictado::join('materias','dictados.id_materia','=','materias.id')
+        /*$dictado = Dictado::join('materias','dictados.id_materia','=','materias.id')
                  ->join('dictados_clases','dictados.id','=','dictados_clases.id_dictado')
                  ->join('carreras','materias.id_carrera','=','carreras.id')
                  ->join('alternativas','dictados_clases.id_alternativa','=','alternativas.id')
                  ->join('dias','dictados_clases.id_dia','=','dias.id')
                  ->select('carreras.id as id_carrera','carreras.desc_carr','dictados.id','materias.id AS id_materia','materias.desc_mat','dictados.cuat','dictados.ano','dias.id AS id_dia','dias.descripcion As dia_cursada','alternativas.id AS id_alternativa','alternativas.codigo AS alt_hor','dictados_clases.id AS id_dictado_clase')
                  ->where('dictados.id','=',$id)
-                 ->get();
+                 ->get();*/
+
+        $dictado = Dictado::join('materias','dictados.id_materia','=','materias.id')
+                 ->join('carreras','materias.id_carrera','=','carreras.id')
+                 ->select('carreras.id as id_carrera','carreras.desc_carr','dictados.id','materias.id AS id_materia','materias.desc_mat','dictados.cuat','dictados.ano')
+                 ->where('dictados.id','=',$id)
+                 ->get();              
 
         return $dictado;
     }
